f5d76676909c632f4d8b0d27ac6ffe02f08d86a0
Zheng Kai <zhengkai@gmail.com>
2020-05-16 19:25:03 +0800

    bootstrap

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './common/routing.module';
+import { BootstrapComponent } from './common/bootstrap.component';
+import { IndexComponent } from './index/index.component';
+import { NavComponent } from './common/nav.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		BootstrapComponent,
+		IndexComponent,
+		NavComponent,
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+	],
+	providers: [],
+	bootstrap: [
+		BootstrapComponent,
+	],
 })
 export class AppModule { }
---
+++ client/src/app/common/bootstrap.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-root',
+	templateUrl: './bootstrap.component.html',
+	styleUrls: ['./bootstrap.component.scss']
+})
+export class BootstrapComponent {
+
+	constructor() { }
+}
